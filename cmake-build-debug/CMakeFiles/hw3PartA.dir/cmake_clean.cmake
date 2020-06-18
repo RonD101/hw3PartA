fa91406cfdc7347ad6088f779db122a8dacde5f6
rond <ron.dahan@campus.technion.ac.il>
2020-06-18 20:51:14 +0300

    f

---
+++ cmake-build-debug/CMakeFiles/hw3PartA.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw3PartA.dir/main.cpp.o"
+  "hw3PartA"
+  "hw3PartA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw3PartA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
